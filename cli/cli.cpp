5f06c1e390f861e56abfae10f2b8b1a67dcd9f80
David Grayson <davidegrayson@gmail.com>
2015-11-04 08:59:46 -0800

    Successfully detected a TREZOR using hidapi.

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -1,8 +1,29 @@
 #include <libhwbtc.hpp>
 #include <iostream>
+#include <cassert>
+#include <hidapi/hidapi.h>
 
 int main(int argc, char ** argv)
 {
     std::cout << "hello there" << std::endl;
+
+    struct hid_device_info * list = hid_enumerate(0x534c, 0x0001);
+
+    struct hid_device_info * info = list;
+    while (info != NULL)
+    {
+        printf("%04x:%04x %ls\n", info->vendor_id, info->product_id, info->product_string);
+        break;
+        info = info->next;
+    }
+
+    if (info == NULL)
+    {
+        std::cerr << "Error: Could not find matching device." << std::endl;
+        return 1;
+    }
+
+    hid_free_enumeration(list);
+
     return 0;
 }
